facaa8f0f0b4fc953ca708cb3f67bb9d1d88add5
Jack, Gao <rgao@litle.com>
2012-03-06 15:44:49 -0500

    working crude version of auth

--- app/Controller/AuthsController.php
+++ app/Controller/AuthsController.php
@@ -1,4 +1,7 @@
 <?php
+error_reporting(E_ALL ^ E_NOTICE);
+require_once realpath(dirname(__FILE__)) . '/../Lib/litle/LitleOnline.php';
+
 App::uses('AppController', 'Controller');
 /**
  * Auths Controller
@@ -41,6 +44,28 @@ class AuthsController extends AppController {
 		if ($this->request->is('post')) {
 			$this->Auth->create();
 			if ($this->Auth->save($this->request->data)) {
+				
+			$hash_in = array(
+			'orderId'=> '4',
+			'amount'=>$this->data['Auth']['amount'],
+			'orderSource'=>'ecommerce',
+			'billToAddress'=>array(
+					'name'=>$this->data['Auth']['name'],
+					'addressLine1'=>$this->data['Auth']['address1'],
+					'city'=>$this->data['Auth']['city'],
+					'state'=>$this->data['Auth']['state'],
+					'country'=>$this->data['Auth']['country'],
+					'zip'=>$this->data['Auth']['zip'],
+					'email'=>$this->data['Auth']['email']),
+			'card'=> array(
+					'type'=>$this->data['Auth']['type'],
+					'number'=>$this->data['Auth']['number'],
+					'expDate'=>$this->data['Auth']['expDate'],
+					'cardValidationNum'=>$this->data['Auth']['cardValidationNum']));
+			$initilaize = &new LitleOnlineRequest();
+			$authorizationResponse = $initilaize->authorizationRequest($hash_in);
+			$message= XmlParser::getAttribute($authorizationResponse,'litleOnlineResponse','message');
+			echo $message;
 				$this->Session->setFlash(__('The auth has been saved'));
 				$this->redirect(array('action' => 'index'));
 			} else {
